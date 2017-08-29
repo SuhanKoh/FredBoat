bb88497a40464a94337bd9c22e8b4512642b8ee3
Napster <napster@npstr.space>
2017-08-29 04:38:37 +0200

    Add Youtube API as fallback solution for patrons
    Ratelimit Spotify playlists to be loaded singlethreaded.

--- FredBoat/src/main/java/fredboat/Config.java
+++ FredBoat/src/main/java/fredboat/Config.java
@@ -303,6 +303,14 @@ public class Config {
         return distribution;
     }
 
+    public boolean isPatronDistribution() {
+        return distribution == DistributionEnum.PATRON;
+    }
+
+    public boolean isDevDistribution() {
+        return distribution == DistributionEnum.DEVELOPMENT;
+    }
+
     public String getBotToken() {
         return botToken;
     }
